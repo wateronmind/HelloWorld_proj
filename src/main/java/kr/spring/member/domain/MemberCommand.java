267804a384e5a0e1de765c7348da59ebfa36cea3
aaa <aaa@aaa.com>
2019-03-29 17:27:28 +0900

    SephPark
    MemberCommand 작성

--- src/main/java/kr/spring/member/domain/MemberCommand.java
+++ src/main/java/kr/spring/member/domain/MemberCommand.java
@@ -9,113 +9,132 @@ import org.hibernate.validator.constraints.NotEmpty;
 
 public class MemberCommand {
 	@NotEmpty
-	private String id;
-	private int auth;
+	private String user_id;
+	private int user_auth;
 	@NotEmpty
-	private String name;
-	@Size(min=4,max=10)
-	private String passwd;
+	private String user_nm;
+	@Size(min=4,max=20)
+	private String user_pw;
 	@NotEmpty
-	private String phone;
+	private String user_phone;
 	@Email
 	@NotEmpty
-	private String email;
-	@Size(min=5,max=5)
-	private String zipcode;
-	@NotEmpty
-	private String address1;
-	private String address2;
-	private Date reg_date;
+	private String user_email;
+	private Date user_apply_dt;
+	private String user_guide_apply;
+	private int user_mil;
+	private byte[] user_profile; // DB에 저장된 파일
+	private String user_filename; // 파일명
 	
-	@Size(min=4,max=10)
+	@Size(min=4,max=20)
 	private String old_passwd;
 	
 	// 비밀번호 일치 여부 체크
 	public boolean isCheckedPasswd(String user_passwd) {
-		if (auth > 0 && passwd.equals(user_passwd)) {
+		if (user_auth > 0 && user_pw.equals(user_passwd)) {
 			return true;
 		}
 		return false;
 	}
 	
-	public String getId() {
-		return id;
+	
+
+	@Override
+	public String toString() {
+		return "MemberCommand [user_id=" + user_id + ", user_auth=" + user_auth + ", user_nm=" + user_nm + ", user_pw="
+				+ user_pw + ", user_phone=" + user_phone + ", user_email=" + user_email + ", user_apply_dt="
+				+ user_apply_dt + ", user_guide_apply=" + user_guide_apply + ", user_mil=" + user_mil
+				+ ", user_filename=" + user_filename + ", old_passwd=" + old_passwd + "]";
 	}
 
-	public void setId(String id) {
-		this.id = id;
+
+
+	public String getUser_id() {
+		return user_id;
 	}
 
-	public int getAuth() {
-		return auth;
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
 	}
 
-	public void setAuth(int auth) {
-		this.auth = auth;
+	public int getUser_auth() {
+		return user_auth;
 	}
 
-	public String getName() {
-		return name;
+	public void setUser_auth(int user_auth) {
+		this.user_auth = user_auth;
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String getUser_nm() {
+		return user_nm;
 	}
 
-	public String getPasswd() {
-		return passwd;
+	public void setUser_nm(String user_nm) {
+		this.user_nm = user_nm;
 	}
 
-	public void setPasswd(String passwd) {
-		this.passwd = passwd;
+	public String getUser_pw() {
+		return user_pw;
 	}
 
-	public String getPhone() {
-		return phone;
+	public void setUser_pw(String user_pw) {
+		this.user_pw = user_pw;
 	}
 
-	public void setPhone(String phone) {
-		this.phone = phone;
+	public String getUser_phone() {
+		return user_phone;
 	}
 
-	public String getEmail() {
-		return email;
+	public void setUser_phone(String user_phone) {
+		this.user_phone = user_phone;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public String getUser_email() {
+		return user_email;
 	}
 
-	public String getZipcode() {
-		return zipcode;
+	public void setUser_email(String user_email) {
+		this.user_email = user_email;
 	}
 
-	public void setZipcode(String zipcode) {
-		this.zipcode = zipcode;
+	public Date getUser_apply_dt() {
+		return user_apply_dt;
 	}
 
-	public String getAddress1() {
-		return address1;
+	public void setUser_apply_dt(Date user_apply_dt) {
+		this.user_apply_dt = user_apply_dt;
 	}
 
-	public void setAddress1(String address1) {
-		this.address1 = address1;
+	public String getUser_guide_apply() {
+		return user_guide_apply;
 	}
 
-	public String getAddress2() {
-		return address2;
+	public void setUser_guide_apply(String user_guide_apply) {
+		this.user_guide_apply = user_guide_apply;
 	}
 
-	public void setAddress2(String address2) {
-		this.address2 = address2;
+	public int getUser_mil() {
+		return user_mil;
 	}
 
-	public Date getReg_date() {
-		return reg_date;
+	public void setUser_mil(int user_mil) {
+		this.user_mil = user_mil;
 	}
 
-	public void setReg_date(Date reg_date) {
-		this.reg_date = reg_date;
+	public byte[] getUser_profile() {
+		return user_profile;
+	}
+
+	public void setUser_profile(byte[] user_profile) {
+		this.user_profile = user_profile;
+	}
+
+	public String getUser_filename() {
+		return user_filename;
+	}
+
+	public void setUser_filename(String user_filename) {
+		this.user_filename = user_filename;
 	}
 
 	public String getOld_passwd() {
@@ -126,11 +145,6 @@ public class MemberCommand {
 		this.old_passwd = old_passwd;
 	}
 	
-	@Override
-	public String toString() {
-		return "MemberCommand [id=" + id + ", auth=" + auth + ", name=" + name + ", passwd=" + passwd + ", phone="
-				+ phone + ", email=" + email + ", zipcode=" + zipcode + ", address1=" + address1 + ", address2="
-				+ address2 + ", reg_date=" + reg_date + ", old_passwd=" + old_passwd + "]";
-	}	
+	
 	
 }
