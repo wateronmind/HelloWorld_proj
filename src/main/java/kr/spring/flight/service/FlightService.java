f3225a13a3b717d5321001fedf173bf40da28d79
sunwookpark <suywook91@naver.com>
2019-04-12 10:13:00 +0900

    박선욱 2019-04-12 10:12
    항공권 추가

--- src/main/java/kr/spring/flight/service/FlightService.java
+++ src/main/java/kr/spring/flight/service/FlightService.java
@@ -4,10 +4,23 @@ import java.util.List;
 import java.util.Map;
 
 import kr.spring.flight.domain.FlightCommand;
+import kr.spring.flight.domain.FlightPayCommand;
+import kr.spring.flight.domain.FlightRsrvCommand;
 
 public interface FlightService {
 	// 항공권
 	public List<FlightCommand> selectFlightList(Map<String, Object> map);
-	public int selectFlightCount(Map<String, Object> map);
-	public void insertFlight(FlightCommand flight);
+	public int selectFlightRowCount(Map<String, Object> map);
+	//public FlightCommand selectFlight(Integer fsi_idx);
+	
+	//항공권 아이디
+	public int getFr_id();
+	//항공권 예약	
+	public void insertFlightRsrv(FlightRsrvCommand flightRsrv);	
+	public FlightCommand selectFlight(Integer fsi_idx);
+	
+	//결제
+	public void insertFlightPay(FlightPayCommand flightPay);
+	
 }
+  
